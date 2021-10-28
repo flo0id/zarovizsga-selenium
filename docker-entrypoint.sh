ff610ed15cbe6f5ee13e0fc9553850df6ae5c1b7
Filyó Dominik <filyodomcsi@gmail.com>
2021-10-28 16:55:27 +0200

    Changes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sleep 5
-selenium-side-runner -s http://chrome:4444 --output-directory /root/out /sides/*.side
+selenium-side-runner -s http://chrome:5555 --output-directory /root/out /sides/*.side
