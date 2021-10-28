657d8d457056b105b8c682bf43d9d65fee80dc41
Filyó Dominik <filyodomcsi@gmail.com>
2021-10-28 17:14:44 +0200

    Changes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM node:10-alpine
 RUN npm install -g selenium-side-runner
+RUN npm install -g webdriver-manager
 ADD docker-entrypoint.sh /opt/bin/docker-entrypoint.sh
 RUN chmod +x /opt/bin/docker-entrypoint.sh
 RUN mkdir /sides
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sleep 5
 echo 'Version 0.1'
-selenium-side-runner -s http://chrome:5555 --output-directory /root/out /sides/*.side
+selenium-side-runner -s http://selenium-hub:4444 --output-directory /root/out /sides/*.side
