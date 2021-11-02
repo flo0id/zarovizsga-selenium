d61414e649014a8b678c97f1270021acc6627997
Filyó Dominik <filyodomcsi@gmail.com>
2021-11-02 16:42:27 +0100

    Starting to migrate to kubernetes

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM node:10-alpine
 RUN npm install -g selenium-side-runner
-RUN npm install -g webdriver-manager
 ADD docker-entrypoint.sh /opt/bin/docker-entrypoint.sh
 RUN chmod +x /opt/bin/docker-entrypoint.sh
 RUN mkdir /sides
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-sleep 5
 echo 'Version 0.1'
-selenium-side-runner -s http://selenium-hub:4444 --output-directory /root/out /sides/*.side
+selenium-side-runner --server http://selenium-hub:4444/wd/hub --output-directory /root/out /sides/*.side
